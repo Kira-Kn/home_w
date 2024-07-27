edebfb240452bf002d0eeba6cdbbeec06711f230
Kira-Kn <zutzumika@gmail.com>
2024-07-27 09:53:30 +0300

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -1,4 +1,1 @@
-def ignore_command (command): 
-    res = lambda: ('True' if word in command else 'False' for word in ['alias','configuration', 'ip', 'sql', 'select', 'update', 'exec','del', 'truncate'])
-
-print(ignore_command('del'))
+def ignore_command (command): lambda: ('True' if word in command else 'False' for word in ['alias','configuration', 'ip', 'sql', 'select', 'update', 'exec','del', 'truncate'])
